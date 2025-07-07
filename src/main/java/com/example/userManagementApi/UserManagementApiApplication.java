02865b7b78399de7c8aabc0581b0758722d5de4a
Priyanshusahay12222301 <119564402+Priyanshusahay12222301@users.noreply.github.com>
2025-07-07 09:23:36 +0530

    first commit

---
+++ src/main/java/com/example/userManagementApi/UserManagementApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.userManagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UserManagementApiApplication.class, args);
+	}
+
+}
